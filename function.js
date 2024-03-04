105342dc2ba04231d8156c4376eb8b79f23f42ae
vivianritah <143530424+vivianritah@users.noreply.github.com>
2024-03-04 12:26:45 +0300

    coursework

---
+++ function.js
@@ -0,0 +1,20 @@
+// part one
+function sum(a, b) {
+    return a + b;
+}
+console.log (sum(8, 9))
+
+                            // part two
+function square(x) {
+    return x * x;
+  }
+console.log(square(5))
+
+                                          // part three
+for (let i = 1; i <= 10; i++) {
+    let squared = square(i)
+    console.log(`The square of ${i}  is ${squared}`);
+  }
+
+
+  
